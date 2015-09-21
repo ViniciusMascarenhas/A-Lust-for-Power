ff9cf9b5697e7da92ef3b102ae1c40d4c1890497
Vinicius <vinicius.mascarenhas@icloud.com>
2015-09-21 19:44:35 -0300

    Organizando um pouco as pastas.

---
+++ Source & Headers/main.cpp
@@ -0,0 +1,15 @@
+// 1.0 
+
+#include "Includes.h"
+#include "SaveState.h"
+
+int main () 
+{
+	SaveState ss;
+	ss.load_file();	
+	
+	//printf("\n");
+	printf("Temples: %d\nHearts: %.2f\nHeart Containers: %d\nMP: %d\nMax MP: %d\nRupees: %d\n", ss.get_temples(), ss.get_hearts(), ss.get_heart_containters(), ss.get_mp(), ss.get_max_mp(), ss.get_rupees());
+	
+	return 0;
+};
