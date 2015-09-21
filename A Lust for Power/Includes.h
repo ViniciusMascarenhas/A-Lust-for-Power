7ba95c950c4c615cdd98f7c353fe3ca6da8b1ca0
Vinicius <vinicius.mascarenhas@icloud.com>
2015-09-21 18:59:43 -0300

    Uploading as classes SaveState e TileBucket.

---
+++ A Lust for Power/Includes.h
@@ -0,0 +1,22 @@
+#ifndef Load_test_Includes_h
+#define Load_test_Includes_h
+
+#include <cstring>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+#define GPATH
+#define LPATH
+#define RPATH
+#define VPATH "/Users/Lobinho/Desktop/Load test/"
+
+#define TILE 16
+
+/*
+#include "SaveState.h"
+#include "TileBucket.h"
+*/
+
+#define FILE_PATH VPATH
+#endif
---
+++ A Lust for Power/TileBucket.h
@@ -0,0 +1,47 @@
+#ifndef __Load_test__TileBucket__
+#define __Load_test__TileBucket__
+
+#include "Includes.h"
+
+class TileBucket
+{
+private:
+	
+	int **matrix;
+	
+public:
+	
+	TileBucket();
+	void Rect (int tx, int ty, int x, int y, int w, int h);
+	void Row (int tx, int ty, int x, int y, int w);
+	void Col (int tx, int ty, int x, int y, int h);
+	void Sq (int tx, int ty, int x, int y, int s);
+	void As_is (int tx, int ty, int x, int y, int w, int h);
+	void Stretch (int tx, int ty, int x, int y, int rx, int ry, int w, int h);
+};
+
+TileBucket::TileBucket()
+{
+		
+};
+
+void TileBucket::Rect (int tx, int ty, int x, int y, int w, int h)
+{
+	//graphics.DrawImage2D(x, y, w * TILE, h * TILE, tx * TILE, ty * TILE, w * TILE, h * TILE, Image image);
+};
+
+#endif /* defined(__Load_test__TileBucket__) */
+
+
+/*
+Funções de construção de tile único:
+Rect (tx, ty, w, h, x, y)
+Row (tx, ty, w, x, y)
+Col (tx, ty, h, x, y)
+Sq (tx, ty, s, x, y)
+
+Funções de construção de múltiplos tiles:
+As_is (tx, ty, w, h, x, y)
+Stretch (tx, ty, w, h, rx, ry, x, y)
+*/
+
