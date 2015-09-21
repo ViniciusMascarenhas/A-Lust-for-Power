02a9b086568c9496e7b86fb5dc89280091cce005
Vinicius <vinicius.mascarenhas@icloud.com>
2015-09-21 19:55:49 -0300

    Organizando as pastas.

--- A Lust for Power/A Lust for Power/Tileset_Parser.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Tileset_Parser.cpp
-//  A Lust for Power
-//
-//  Created by Vinícius Mascarenhas on 2015-09-20.
-//  Copyright (c) 2015 Vinícius Mascarenhas. All rights reserved.
-//
-
-#include "Tileset_Parser.h"
