5e297b648f5117b9f6528ac10426bfa709e437b5
Vinicius Mascarenhas <vinicius.mascarenhas@icloud.com>
2015-10-23 12:54:19 -0200

    Corrigindo Includes. Talvez não o usemos.

--- Source & Headers/Includes.h
+++ Source & Headers/Includes.h
@@ -3,24 +3,26 @@
 #ifndef Load_test_Includes_h
 #define Load_test_Includes_h
 
+//#include "Graphics.h"
 #include <cstring>
 #include <stdlib.h>
 #include <stdio.h>
-using namespace std;
 
 /* Os #defines abaixo devem ter os caminhos para as pastas nas suas máquinas, incluindo
 "\\" no final. Não mexer nos #defines dos coleguinhas! */
 
-#define GPATH	""
-#define LBPATH	""
-#define LFPATH	""
-#define RPATH	""
-#define VPATH	"C:\\Users\\Admin\\Desktop\\ALFP\\Source & Headers\\"
+#define GABRIEL_PATH	""
+#define LEONARDO_PATH	""
+#define LUCCA_PATH		""
+#define ROBERTO_PATH	""
+#define VINICIUS_PATH	"C:\\Users\\Admin\\Documents\\GitHub\\A-Lust-for-Power\\Source & Headers\\"
 
 /* No #define abaixo, trocar o símbolo da direita pela constante definida correspondente
-ao caminho da pasta Source & Headers do projeto na sua máquina.*/
+ao caminho da pasta Source & Headers do projeto na sua máquina.
+Quem usar mais de uma máquina deve criar defines adicionais nas linhas acima para trocar o símbolo
+abaixo. Por exemplo: FULANO_PATH_DESKTOP, FULANO_PATH_LAPTOP, etc. */
 
-#define FILE_PATH VPATH
+#define FILE_PATH VINICIUS_PATH
 
 #define TILE 16
 
