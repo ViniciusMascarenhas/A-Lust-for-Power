18f371c77a85a5de3828ae7080f39ef930f1aba8
Vinicius Mascarenhas <vinicius.mascarenhas@icloud.com>
2015-10-28 12:35:19 -0200

    Main nova.

--- Source & Headers/main.cpp
+++ Source & Headers/main.cpp
@@ -38,14 +38,14 @@ void MainLoop(void)
 
 int main (void)
 {
-	//save_state.print_table();						// Exibe na tela o arquivo de exemplo SaveFile.txt.
+	save_state.print_table();						// Exibe na tela o arquivo de exemplo SaveFile.txt.
 	
 	graphics.CreateMainWindow(640, 480, "A Lust for Power");
 	graphics.SetBackgroundColor(32,32,40);
 	
 	char image_path[512];							// Para guardar o caminho completo do nome do arquivo de imagem.
 	path (image_path, CR, "Ganon.png");				// Usa os defines em Defines.h para concatenar o caminho completo.
-	if (0) printf("Test:\t\t%s\n\n", image_path);	// Exibe o caminho para verificar se está correto -- e está.
+	if (1) printf("Test:\t\t%s\n\n", image_path);	// Exibe o caminho para verificar se está correto -- e está.
 	image.LoadPNGImage(image_path);					// Problema aqui ou na chamada de Graphics::DrawImage2D(...).
 	
 	image_in_same_folder.LoadPNGImage("Ganon.png");	// Esta imagem está na mesma pasta do código do projeto.
